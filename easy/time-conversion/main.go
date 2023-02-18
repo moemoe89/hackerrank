391cdc56dee7a6a69cc9cbecf0a57d8da9a29d1f
Bismo Baruno <bismobaruno@gmail.com>
2023-02-18 22:01:01 +0900

    feat: add time conversion

---
+++ easy/time-conversion/main.go
@@ -0,0 +1,52 @@
+package main
+
+import "fmt"
+
+func timeConversion(s string) string {
+	n := len(s)
+
+	r := []rune(s)
+
+	if s[n-2] == 'P' {
+
+		x := int(s[0]-'0') * 10
+		x2 := (int(s[1] - '0')) + x
+
+		if x2 == 12 {
+			goto result
+		}
+
+		h := 0
+
+		if int(s[0]-'0') > 0 {
+
+			h = int(s[0]-'0') * 10
+			h += int(s[1] - '0')
+			h += 12
+
+		} else {
+			h = int(s[1] - '0')
+			h += 12
+		}
+
+		r[0] = '0' + rune(h/10)
+		r[1] = '0' + rune(h%10)
+	} else {
+
+		x := int(s[0]-'0') * 10
+		x2 := (int(s[1] - '0')) + x
+
+		if x2 == 12 {
+			r[0] = '0' + rune(0)
+			r[1] = '0' + rune(0)
+		}
+	}
+
+result:
+
+	return string(r[:n-2])
+}
+
+func main() {
+	fmt.Println(timeConversion("07:05:45PM"))
+}
