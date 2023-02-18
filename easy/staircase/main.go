fea27aa37b050ad71205e59a0c171c286f5f7072
Bismo Baruno <bismobaruno@gmail.com>
2023-02-18 21:38:19 +0900

    feat: add staircase

---
+++ easy/staircase/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func staircase(n int32) {
+	for i := int32(0); i < n; i++ {
+
+		for j := int32(0); j < n; j++ {
+			if j >= n-i-1 {
+				fmt.Print("#")
+			} else {
+				fmt.Print(" ")
+			}
+		}
+
+		fmt.Println("")
+	}
+}
+
+func main() {
+	staircase(6)
+}
