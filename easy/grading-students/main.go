deb53be276c0db9b0f8ceba836b4f9ba694041b8
Bismo Baruno <bismobaruno@gmail.com>
2023-02-18 22:13:46 +0900

    feat: grading students

---
+++ easy/grading-students/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func gradingStudents(grades []int32) []int32 {
+	for i := range grades {
+		if grades[i] < 38 {
+			continue
+		}
+
+		target := grades[i] / 5
+		target *= 5
+		target += 5
+
+		if target-grades[i] < 3 {
+			grades[i] = target
+		}
+	}
+
+	return grades
+}
+
+func main() {
+	fmt.Println(gradingStudents([]int32{73, 67, 38, 33}))
+}
