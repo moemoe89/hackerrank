b5a1d15f3f81b41b73521688f5fd4314e05fa9d3
Bismo Baruno <bismobaruno@gmail.com>
2023-02-18 23:17:43 +0900

    feat: add find digits

---
+++ easy/find-digits/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func findDigits(n int32) int32 {
+	output := int32(0)
+
+	num := n
+
+	for n != 0 {
+		m := n % 10
+
+		if m != 0 && num%m == 0 {
+			output++
+		}
+
+		n /= 10
+	}
+
+	return output
+}
+
+func main() {
+	fmt.Println(findDigits(1012))
+}
