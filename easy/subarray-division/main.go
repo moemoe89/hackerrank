845d16b64666784ad2725cd64e83e4718415c47a
Bismo Baruno <bismobaruno@gmail.com>
2023-02-19 16:06:27 +0900

    feat: add subarray division

---
+++ easy/subarray-division/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func birthday(s []int32, d int32, m int32) int32 {
+	var out int32
+
+	for k, _ := range s {
+		var i, sum int32
+
+		for i = 0; i < m; i++ {
+			j := k + int(i)
+
+			if j > len(s)-1 {
+				break
+			}
+
+			sum += s[j]
+		}
+
+		if sum == d {
+			out++
+		}
+	}
+
+	return out
+}
+
+func main() {
+	fmt.Println(birthday([]int32{1, 2, 1, 3, 2}, 3, 2))
+}
