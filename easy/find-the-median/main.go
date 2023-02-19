fcaf88733dc185a8a4fcc55b87d543d4c3e15848
Bismo Baruno <bismobaruno@gmail.com>
2023-02-19 21:00:58 +0900

    feat: find the median

---
+++ easy/find-the-median/main.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+func findMedian(arr []int32) int32 {
+	n := len(arr)
+
+	quickSort(arr, 0, n-1)
+
+	mid := n / 2
+
+	return arr[mid]
+}
+
+func quickSort(arr []int32, left int, right int) {
+	if left < right {
+		pivotIndex := partition(arr, left, right)
+		quickSort(arr, left, pivotIndex-1)
+		quickSort(arr, pivotIndex+1, right)
+	}
+}
+
+func partition(arr []int32, left int, right int) int {
+	pivot := arr[right]
+
+	i := left
+
+	for j := left; j < right; j++ {
+		if arr[j] < pivot {
+			arr[i], arr[j] = arr[j], arr[i]
+			i++
+		}
+	}
+
+	arr[i], arr[right] = arr[right], arr[i]
+
+	return i
+}
+
+func main() {
+	fmt.Println(findMedian([]int32{0, 1, 2, 4, 6, 5, 3}))
+}
