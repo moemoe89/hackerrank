c11ecc0e7d83b48aa1d2b02d0a146ed683dce431
Bismo Baruno <bismobaruno@gmail.com>
2023-02-19 21:14:49 +0900

    feat: add missing numbers

---
+++ easy/missing-numbers/main.go
@@ -0,0 +1,62 @@
+package main
+
+import "fmt"
+
+func missingNumbers(arr []int32, brr []int32) []int32 {
+	var out []int32
+
+	m := make(map[int32]int)
+
+	for i := range arr {
+		m[arr[i]]++
+	}
+
+	for i := range brr {
+		m[brr[i]]--
+	}
+
+	for k, v := range m {
+		if v == 0 {
+			continue
+		}
+
+		out = append(out, k)
+	}
+
+	quickSort(out, 0, len(out)-1)
+
+	return out
+}
+
+func quickSort(arr []int32, left int, right int) {
+	if left < right {
+		pivotIndex := partition(arr, left, right)
+		quickSort(arr, left, pivotIndex-1)
+		quickSort(arr, pivotIndex+1, right)
+	}
+}
+
+func partition(arr []int32, left int, right int) int {
+	pivot := arr[right]
+
+	i := left
+
+	for j := left; j < right; j++ {
+		if arr[j] < pivot {
+			arr[i], arr[j] = arr[j], arr[i]
+			i++
+		}
+	}
+
+	arr[i], arr[right] = arr[right], arr[i]
+
+	return i
+}
+
+func main() {
+	fmt.Println(missingNumbers(
+		[]int32{11, 4, 11, 7, 13, 4, 12, 11, 10, 14},
+		[]int32{11, 4, 11, 7, 3, 7, 10, 13, 4, 8, 12, 11, 10, 14, 12},
+	),
+	)
+}
