f1b054e01c9f79816f35c01b2ba8f5be4fd9e18b
Bismo Baruno <bismobaruno@gmail.com>
2023-02-19 16:23:23 +0900

    feat: add the hurdle race

---
+++ easy/the-hurdle-race/main.go
@@ -0,0 +1,46 @@
+package main
+
+import "fmt"
+
+func hurdleRace(k int32, height []int32) int32 {
+	n := len(height) - 1
+
+	quickSort(height, 0, n)
+
+	maxNumb := height[n]
+	if k > maxNumb {
+		return 0
+	}
+
+	out := maxNumb - k
+	return out
+}
+
+func quickSort(arr []int32, left int, right int) {
+	if left < right {
+		pivotIndex := partition(arr, left, right)
+		quickSort(arr, left, pivotIndex-1)
+		quickSort(arr, pivotIndex+1, right)
+	}
+}
+
+func partition(arr []int32, left int, right int) int {
+	pivot := arr[right]
+
+	i := left
+
+	for j := left; j < right; j++ {
+		if arr[j] < pivot {
+			arr[i], arr[j] = arr[j], arr[i]
+			i++
+		}
+	}
+
+	arr[i], arr[right] = arr[right], arr[i]
+
+	return i
+}
+
+func main() {
+	fmt.Println(hurdleRace(1, []int32{1, 2, 3, 3, 2}))
+}
