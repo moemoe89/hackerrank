7af035ba0d606ea7a6008e1843303fe04569a4b2
Bismo Baruno <bismobaruno@gmail.com>
2023-02-19 16:49:42 +0900

    feat: add viral advertising

---
+++ easy/viral-advertising/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func viralAdvertising(n int32) int32 {
+	var out, likes, i int32
+
+	out = 2
+	likes = 2
+	
+	for i = 2; i <= n; i++ {
+		adv := likes * 3
+		halfAdv := adv / 2
+		out += halfAdv
+		likes = halfAdv
+	}
+
+	return out
+}
+
+func main() {
+	fmt.Println(viralAdvertising(3))
+}
