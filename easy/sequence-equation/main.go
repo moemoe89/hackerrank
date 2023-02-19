62f023607504030f1b6eb92b1ab829dcf8726dac
Bismo Baruno <bismobaruno@gmail.com>
2023-02-19 16:54:00 +0900

    feat: add sequence equation

---
+++ easy/sequence-equation/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func permutationEquation(p []int32) []int32 {
+	var out []int32
+
+	mapP := make(map[int32]int32)
+	for k, v := range p {
+		mapP[v] = int32(k + 1)
+	}
+
+	for k, _ := range p {
+		k = k + 1
+		out = append(out, mapP[mapP[int32(k)]])
+	}
+
+	return out
+}
+
+func main() {
+	fmt.Println(permutationEquation([]int32{2, 3, 1}))
+}
