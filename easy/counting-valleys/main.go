6c388cb44ec3de8b1c39afaaafbee2ad5b84fa2c
Bismo Baruno <bismobaruno@gmail.com>
2023-02-19 16:19:23 +0900

    feat: add counting valleys

---
+++ easy/counting-valleys/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func countingValleys(n int32, s string) int32 {
+	var output int32
+
+	position := 0
+
+	for _, r := range s {
+		if r == 'U' {
+			position += 1
+		} else if r == 'D' {
+			position -= 1
+		}
+
+		if position == 0 {
+			if r == 'U' {
+				output++
+			}
+		}
+	}
+
+	return output
+}
+
+func main() {
+	fmt.Println(countingValleys(8, "UDDDUDUU"))
+}
