e0e3ce0ec3ce0054a0ae1b15f1831b318f5863f2
Bismo Baruno <bismobaruno@gmail.com>
2023-02-19 16:08:17 +0900

    feat: add divisible sum pairs

---
+++ easy/divisible-sum-pairs/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func divisibleSumPairs(n int32, k int32, ar []int32) int32 {
+	output := int32(0)
+
+	for i := 0; i < int(n); i++ {
+		for j := i + 1; j < int(n); j++ {
+			if (ar[i]+ar[j])%k == 0 {
+				output++
+			}
+		}
+	}
+
+	return output
+}
+
+func main() {
+	fmt.Println(divisibleSumPairs(6, 3, []int32{1, 3, 2, 6, 1, 2}))
+}
