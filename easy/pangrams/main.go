519272080057baa437b9cfc5f6244f7cb75f44b1
Bismo Baruno <bismobaruno@gmail.com>
2023-03-10 23:13:03 +0900

    feat: add pangrams

---
+++ easy/pangrams/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func pangrams(s string) string {
+	mapS := make(map[rune]struct{}, 0)
+
+	for _, r := range s {
+		if r >= 'A' && r <= 'Z' {
+			r = r - 'A' + 'a'
+		}
+
+		if r >= 'a' && r <= 'z' {
+			mapS[r] = struct{}{}
+		}
+	}
+
+	if len(mapS) == 26 {
+		return "pangram"
+	}
+
+	return "not pangram"
+}
+
+func main() {
+	fmt.Println(pangrams("We promptly judged antique ivory buckles for the prize"))
+}
