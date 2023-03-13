135ec657a551f42fca9f9da106b2c1e751c3d22e
Bismo Baruno <bismobaruno@gmail.com>
2023-03-13 10:56:15 +0900

    feat: add funny string

---
+++ easy/funny-string/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func funnyString(s string) string {
+	n := len(s) - 1
+
+	for i := range s {
+		if i < n {
+			diff1 := int(s[i+1]) - int(s[i])
+
+			diff2 := int(s[n-i]) - int(s[n-i-1])
+
+			if diff1 < 0 {
+				diff1 *= -1
+			}
+
+			if diff2 < 0 {
+				diff2 *= -1
+			}
+
+			if diff1 != diff2 {
+				return "Not Funny"
+			}
+		}
+	}
+
+	return "Funny"
+}
+
+func main() {
+	fmt.Println(funnyString("acxz"))
+}
