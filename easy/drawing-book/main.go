ca9d7d56419bc6ba3c9acac1127ceeb231ae18bd
Bismo Baruno <bismobaruno@gmail.com>
2023-03-13 13:15:58 +0900

    feat: add drawing book

---
+++ easy/drawing-book/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func pageCount(n int32, p int32) int32 {
+	front := p / 2
+
+	back := (n - p) / 2
+
+	if n%2 == 0 {
+		back = (n + 1 - p) / 2
+	}
+
+	if front < back {
+		return front
+	}
+
+	return back
+}
+
+func main() {
+	fmt.Println(pageCount(6, 2))
+}
