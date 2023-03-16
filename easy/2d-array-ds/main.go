77010782a2be5f1eeaa24aec64e950b07fcf9827
Bismo Baruno <bismobaruno@gmail.com>
2023-03-16 21:48:48 +0900

    feat: add 2d array ds

---
+++ easy/2d-array-ds/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func hourglassSum(arr [][]int32) int32 {
+	count := int32(-63)
+	sum := int32(0)
+
+	for i := 0; i <= 3; i++ {
+
+		for j := 0; j <= 3; j++ {
+			sum = arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2]
+
+			if sum > count {
+				count = sum
+			}
+		}
+
+	}
+
+	return count
+}
+
+func main() {
+	fmt.Println(hourglassSum([][]int32{
+		{1, 1, 1, 0, 0, 0},
+		{0, 1, 0, 0, 0, 0},
+		{1, 1, 1, 0, 0, 0},
+		{0, 0, 2, 4, 4, 0},
+		{0, 0, 0, 2, 0, 0},
+		{0, 0, 1, 2, 4, 0},
+	}))
+}
