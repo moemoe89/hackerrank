2c2f8b1f8b46373f24ddb304a1c45b73a3eb86e0
Bismo Baruno <bismobaruno@gmail.com>
2023-03-18 22:32:46 +0900

    feat: add reverse a doubly linked list

---
+++ easy/reverse-a-doubly-linked-list/main.go
@@ -0,0 +1,64 @@
+package main
+
+import "fmt"
+
+type DoublyLinkedListNode struct {
+	data int32
+	next *DoublyLinkedListNode
+	prev *DoublyLinkedListNode
+}
+
+func reverse(llist *DoublyLinkedListNode) *DoublyLinkedListNode {
+	if llist == nil {
+		return llist
+	}
+
+	for llist != nil {
+		prev := llist.prev
+		llist.prev = llist.next
+		llist.next = prev
+
+		if llist.prev == nil {
+			return llist
+		}
+
+		llist = llist.prev
+	}
+
+	return llist
+}
+
+func main() {
+	first := &DoublyLinkedListNode{
+		data: 1,
+	}
+
+	second := &DoublyLinkedListNode{
+		data: 2,
+	}
+
+	third := &DoublyLinkedListNode{
+		data: 3,
+	}
+
+	fourth := &DoublyLinkedListNode{
+		data: 4,
+	}
+
+	first.next = second
+
+	second.prev = first
+	second.next = third
+
+	third.prev = second
+	third.next = fourth
+
+	fourth.prev = third
+
+	res := reverse(first)
+
+	for res != nil {
+		fmt.Printf("%+v\n", res)
+		res = res.next
+	}
+}
