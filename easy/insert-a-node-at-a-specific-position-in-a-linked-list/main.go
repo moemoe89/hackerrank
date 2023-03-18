da5d55c53a62b2d497fb9ae97e50ca12d4977659
Bismo Baruno <bismobaruno@gmail.com>
2023-03-18 22:19:29 +0900

    feat: add insert a node at a specific position in a linked list

---
+++ easy/insert-a-node-at-a-specific-position-in-a-linked-list/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+)
+
+type SinglyLinkedListNode struct {
+	data int32
+	next *SinglyLinkedListNode
+}
+
+func insertNodeAtPosition(llist *SinglyLinkedListNode, data int32, position int32) *SinglyLinkedListNode {
+	tmp := &SinglyLinkedListNode{
+		data: data,
+	}
+
+	head := llist
+
+	idx := int32(0)
+
+	for idx < position-1 {
+		head = head.next
+		idx++
+	}
+
+	tmp.next = head.next
+	head.next = tmp
+
+	return llist
+}
+
+func main() {
+	llist := &SinglyLinkedListNode{
+		data: 1,
+		next: &SinglyLinkedListNode{
+			data: 2,
+			next: &SinglyLinkedListNode{
+				data: 3,
+			},
+		},
+	}
+
+	res := insertNodeAtPosition(llist, 4, 2)
+
+	for res != nil {
+		fmt.Printf("%+v\n", res)
+		res = res.next
+	}
+}
