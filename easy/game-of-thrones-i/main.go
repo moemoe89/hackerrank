4663b69ab4f2e208ff41360c8c51155d1f1cd1ab
Bismo Baruno <bismobaruno@gmail.com>
2023-03-26 18:14:58 +0900

    feat: add game of thrones i

---
+++ easy/game-of-thrones-i/main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func gameOfThrones(s string) string {
+	// initialize the map.
+	m := make(map[rune]int, 0)
+
+	// store all element and the frequency to map.
+	for _, r := range s {
+		m[r]++
+	}
+
+	// prepare the odd num variable.
+	oddNum := 0
+
+	// iterates the map.
+	for _, v := range m {
+		// checks the frequency odd or not.
+		// if odd increment the value.
+		if v%2 != 0 {
+			oddNum++
+
+			// if odd number found more than 1,
+			// it definitely not palindrome.
+			if oddNum > 1 {
+				return "NO"
+			}
+		}
+	}
+
+	return "YES"
+}
+
+func main() {
+	fmt.Println(gameOfThrones("aaabbbb"))
+}
