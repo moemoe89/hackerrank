f402aa88005cc85d9f418b537747793acd0175b3
Bismo Baruno <bismobaruno@gmail.com>
2023-04-02 10:12:49 +0900

    feat: print in reverse

---
+++ easy/print-in-reverse/main.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+type SinglyLinkedListNode struct {
+	data int32
+	next *SinglyLinkedListNode
+}
+
+func reversePrint(llist *SinglyLinkedListNode) {
+	var values []int32
+
+	// loop until the end of linked list.
+	for {
+		// append to array with reverse position,
+		values = append([]int32{llist.data}, values...)
+
+		// if reach the last element, break the loop.
+		if llist.next == nil {
+			break
+		}
+
+		// override the current (llist) with the next.
+		llist = llist.next
+	}
+
+	for i := range values {
+		fmt.Println(values[i])
+	}
+}
+
+func main() {
+	llist := &SinglyLinkedListNode{
+		data: 1,
+		next: &SinglyLinkedListNode{
+			data: 2,
+			next: &SinglyLinkedListNode{
+				data: 3,
+			},
+		},
+	}
+
+	reversePrint(llist)
+}
