22a606a721f9a6e189a700b41207c959e2fc973f
Bismo Baruno <bismobaruno@gmail.com>
2023-04-16 16:15:31 +0900

    feat: add map to fibonacci

--- easy/recursion-fibonacci-numbers/main.go
+++ easy/recursion-fibonacci-numbers/main.go
@@ -6,13 +6,22 @@ import "fmt"
 // The Fibonacci sequence is the series of numbers where each number is the sum of the two preceding numbers.
 // For example: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610,
 func fibonacci(n int) int {
-	// if n = 0 or n = 1, return n
-	if n <= 1 {
-		return n
+	m := map[int]int{
+		0: 0,
+		1: 1,
 	}
 
-	// do recursive
-	return fibonacci(n-1) + fibonacci(n-2)
+	return fibonacciMap(n, m)
+}
+
+func fibonacciMap(n int, m map[int]int) int {
+	if val, ok := m[n]; ok {
+		return val
+	}
+
+	m[n] = fibonacciMap(n-2, m) + fibonacciMap(n-1, m)
+
+	return m[n]
 }
 
 func main() {
