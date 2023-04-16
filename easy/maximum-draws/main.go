0b8c4c1449f3eaa9d393f63b3efe55309f922de4
Bismo Baruno <bismobaruno@gmail.com>
2023-04-16 22:00:00 +0900

    feat: add maximum draws

---
+++ easy/maximum-draws/main.go
@@ -0,0 +1,12 @@
+// https://www.hackerrank.com/challenges/maximum-draws/problem?isFullScreen=true
+package main
+
+import "fmt"
+
+func maximumDraws(n int32) int32 {
+	return n + 1
+}
+
+func main() {
+	fmt.Println(maximumDraws(2))
+}
