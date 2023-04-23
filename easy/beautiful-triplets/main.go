43a5e8d2faf5f1a3fa9136a6fcaefbce3eae1e27
Bismo Baruno <bismobaruno@gmail.com>
2023-04-23 10:32:29 +0900

    feat: change code

--- easy/beautiful-triplets/main.go
+++ easy/beautiful-triplets/main.go
@@ -5,34 +5,22 @@ import "fmt"
 
 func beautifulTriplets(d int32, arr []int32) int32 {
 	// initialize map to store the elements.
-	m := make(map[int32]int32)
+	m := make(map[int32]bool)
 
 	// store arr element to map.
 	for _, a := range arr {
-		m[a]++
+		m[a] = true
 	}
 
 	// initialize output var
 	output := int32(0)
 
-	// iterates arr backward.
-	for i := len(arr) - 1; i >= 0; i-- {
-		// if the element lower than d,
-		// it's impossible to have the beauty triplets,
-		// then stop it.
-		if arr[i] <= d {
-			break
+	// iterates the arr
+	for _, i := range arr {
+		// check the triplets key exist or not in map
+		if m[i+d] && m[i+d*2] {
+			output++
 		}
-
-		// each pair diff should equal with d,
-		// then we can find the map key based on that each pair diff.
-		p1 := arr[i]
-		p2 := p1 - d
-		p3 := p2 - d
-
-		// if one of map doesn't exists, it will return 0,
-		// then output will added by 0 which not changes anything.
-		output += m[p1] * m[p2] * m[p3]
 	}
 
 	return output
